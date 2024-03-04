b73edcc1fb669128168dd727e474e949aa8c4553
Bohdan Hryhorian <bohdan.hryhorian@playtech.com>
2024-03-04 20:45:00 +0200

    hw_4 ex_1 ex_2 formatting & refactoring

--- hw-04/exercise_1/main.py
+++ hw-04/exercise_1/main.py
@@ -2,7 +2,7 @@ import math
 from typing import Tuple
 
 
-def read_file_save(path: str):
+def read_file_safe(path: str):
     try:
         with open(path, 'r', encoding='utf-8') as fh:
             return [el.strip() for el in fh.readlines() if el.strip()]
@@ -11,19 +11,19 @@ def read_file_save(path: str):
         return []
 
 
-def total_salary(path: str) -> Tuple[float, float]:
-    total = None
-    average = None
+def total_salary(path: str) -> Tuple[int, int]:
+    total_s = 0
+    average_s = 0
 
-    lines = read_file_save(path)
+    lines = read_file_safe(path)
 
     if lines:
         salaries = [int(line.strip().split(',')[1]) for line in lines]
-        total = sum(salaries)
-        average  = total // len(salaries)
+        total_s = sum(salaries)
+        average_s = total // len(salaries)
 
-    return total, average
-    
+    return total_s, average_s
+
     # try:
     #     with open(path, 'r', encoding='utf-8') as fh:
     #         salaries = [int(line.strip().split(',')[1]) for line in fh.readlines() if line.strip()]
@@ -33,7 +33,7 @@ def total_salary(path: str) -> Tuple[float, float]:
     #     print(e)
     # finally:
     #     return total, average
-    
-        
+
+
 total, average = total_salary("hw-04/exercise_1/salaries.txt")
 print(f"Total amount of salaries: {total}\nAverage salary: {average}")
