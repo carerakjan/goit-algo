9a8ee45ed0b5c8d8d400d504526a23909a1339bc
Bo_Hr <carerakjan@gmail.com>
2024-03-05 10:33:04 +0200

    hw_4 ex_3 additional view mode with arg -b

--- hw-04/exercise_3/main.py
+++ hw-04/exercise_3/main.py
@@ -1,41 +1,19 @@
 import sys
 from pathlib import Path
-from colorama import Fore, Back, Style
+from colorama import Fore
+from template import template
 
 
-def show_error(err: str):
-    print(f'{Fore.RED}{err}{Fore.RESET}')
-
-
-def get_colors(is_dir: bool):
-    return Fore.CYAN if is_dir else Fore.GREEN, Fore.RESET
-
-
-def get_assets():
-    return {
-        'ROOT': '📦',
-        'DIR': '📂',
-        'FILE': '📜',
-        'LINE': '┃',
-        'LINE_2': '┣'
-    }
-
-
-def print_path(source_path: Path, depth=0, depth_limit=20):
+def print_path(source_path: Path, beautify: bool, depth=0, depth_limit=20):
     if depth > depth_limit:
         return
 
     is_dir = source_path.is_dir()
-    assets = get_assets()
-    offset = Style.DIM + Fore.CYAN + (assets['LINE'] + ' ') * (depth - 1) + assets[
-        'LINE_2'] + ' ' + Style.RESET_ALL if depth else ''
-    asset = assets['ROOT'] if depth == 0 else assets['DIR'] if is_dir else assets['FILE']
-    color, reset = get_colors(is_dir)
-    print(f'{offset}{asset}{color}{source_path.absolute().name}{reset}')
+    template(source_path.absolute().name, is_dir=is_dir, depth=depth, beautify=beautify)
 
-    if source_path.is_dir():
+    if is_dir:
         for dir_or_file in sorted(source_path.iterdir()):
-            print_path(dir_or_file.absolute(), depth + 1, depth_limit)
+            print_path(dir_or_file.absolute(), beautify, depth + 1, depth_limit)
 
 
 def main():
@@ -44,6 +22,7 @@ def main():
             raise Exception('Path is not specified')
 
         root_path = Path(sys.argv[1])
+        beautify = True if len(sys.argv) > 2 and sys.argv[2] == '-b' else False
 
         if not root_path.exists():
             raise Exception(f'"{root_path}" is not exists')
@@ -51,9 +30,9 @@ def main():
         if not root_path.is_dir():
             raise Exception(f'"{root_path}" is not a directory')
 
-        print_path(root_path)
+        print_path(root_path, beautify)
     except Exception as e:
-        show_error(e)
+        print(f'{Fore.RED}{e}{Fore.RESET}')
 
 
 if __name__ == '__main__':
---
+++ hw-04/exercise_3/template.py
@@ -0,0 +1,28 @@
+from colorama import Fore, Back, Style
+
+def get_colors(is_dir: bool, beautify: bool):
+    if beautify:
+        return Fore.LIGHTYELLOW_EX if is_dir else Fore.LIGHTWHITE_EX, Fore.RESET
+    return Fore.CYAN if is_dir else Fore.GREEN, Fore.RESET
+
+
+def get_assets():
+    return {
+        'ROOT': '📦',
+        'DIR': '📂',
+        'FILE': '📜',
+        'LINE': '┃',
+        'LINE_2': '┣'
+    }
+
+def template(path_name:str, is_dir: bool, depth: int, beautify = False):
+    offset = Style.DIM + Fore.CYAN + '| ' * (depth - 1) + '|_' + Style.RESET_ALL if depth else ''
+    asset = ''
+    
+    if beautify:
+        assets = get_assets()
+        offset = (assets['LINE'] + ' ') * (depth - 1) + assets['LINE_2'] if depth else ''
+        asset = (assets['ROOT'] if depth == 0 else assets['DIR'] if is_dir else assets['FILE']) + ' '
+
+    color, reset = get_colors(is_dir, beautify)
+    print(f'{offset}{asset}{color}{path_name}{reset}')
