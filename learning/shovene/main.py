f1dd5affc78b8cb46ce0debdacafabb05885be01
Bo_Hr <carerakjan@gmail.com>
2024-03-07 17:34:35 +0200

    shovene refactoring

--- learning/shovene/main.py
+++ learning/shovene/main.py
@@ -1,6 +1,15 @@
 import random
 import statistics as st
+import re
+import sys
 
+def get_data():
+    try:
+        with open('./da5ta.txt', 'r') as fh:
+            lines = fh.readlines()
+            return list(map(lambda str: float(str.replace(',', '.')),[line.strip() for line in lines if line.strip()]))
+    except Exception:
+        return []
 
 def generate(amount: int):
     num = []
@@ -10,21 +19,34 @@ def generate(amount: int):
         num.append(round(random.gauss(mu, sigma), 3))
     return num
 
+def parse_coefs(lines, coefs):
+    for line in [lst.split() for lst in lines if re.search('[^a-z]+', (''.join(lst.split())).lower())]:
+        for idx, item in enumerate(line):
+            if (idx % 2 == 0):
+                z = float(item)
+                m = int(line[idx + 1])
+                coefs[z] = m
 
 def get_coefficients():
-    crits = {}
+    coefs = {}
     with open('./shovene_coefs.txt', 'r') as fh:
         lines = fh.readlines()
-        for l in [l.strip() for l in lines]:
-            n, m, *_ = l.split(',')
-            crits[int(n)] = float(m)
-    return crits
+        parse_coefs(lines, coefs)
+
+    return dict(sorted(coefs.items()))
+
+
+def check_miss(val_coef, val_nums, coefs):
+    for coef in coefs.keys():
+        if (val_coef <= coef):
+            return val_nums < coefs[coef]
+        
 
 
 def main():
-    data = generate(30)
-    data_sorted = sorted(data)
-    sh_crits = get_coefficients()
+    data = map(float, sys.argv[1:]) if len(sys.argv) > 1 else (get_data() or generate(30))
+    data_sorted = sorted(list(data))
+    sh_coefs = get_coefficients()
     count = 1
 
     while True:
@@ -36,7 +58,6 @@ def main():
 
         sh_min = (data_mean - data_min) / data_stdev
         sh_max = (data_max - data_mean) / data_stdev
-        sh_crit = sh_crits[data_len]
 
         print(f'-------- ітерація - {count} -----------')
         print('дані: ', data_sorted)
@@ -45,19 +66,18 @@ def main():
         print('стандартне відхилення: ', data_stdev)
         print('мінімільне значення: ', data_min)
         print('максимальне значення: ', data_max)
-        print(f'критичне значення Шовене: ', sh_crit)
         print('коеф. Шовене для мін. значення: ', sh_min)
         print('коеф. Шовене для макс. значення: ', sh_max)
 
         count += 1
         misses = 0
 
-        if sh_min >= sh_crit:
+        if check_miss(sh_min, data_len, sh_coefs):
             print(f'мінімільне значення "{data_min}" є промахом')
             data_sorted = data_sorted[1:]
             misses += 1
 
-        if sh_max >= sh_crit:
+        if check_miss(sh_max, data_len, sh_coefs):
             print(f'максимальне значення "{data_max}" є промахом')
             data_sorted = data_sorted[:-1]
             misses += 1
